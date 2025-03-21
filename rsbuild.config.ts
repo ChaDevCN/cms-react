305177ca0aee7024a2853cc877a05dc2af705596
Charlie <meixiguozao@outlook.com>
2025-03-21 10:41:17 +0800

    feat(global): eslint prettier husky tailwind react rsbuild

---
+++ rsbuild.config.ts
@@ -0,0 +1,23 @@
+import * as path from 'path';
+
+import { defineConfig } from '@rsbuild/core';
+import { pluginLess } from '@rsbuild/plugin-less';
+import { pluginReact } from '@rsbuild/plugin-react';
+const rootDirname = path.resolve(__dirname, '../../');
+export default defineConfig({
+	plugins: [pluginReact(), pluginLess()],
+	resolve: {
+		alias: {
+			'@app/css': path.resolve(rootDirname, './libs/assets/styles'),
+			'@': path.resolve(__dirname, './src')
+		}
+	},
+	html: {
+		template: './static/index.html'
+	},
+	server: {
+		publicDir: {
+			name: './static'
+		}
+	}
+});
---
+++ src/index.tsx
@@ -0,0 +1,33 @@
+import { ConfigProvider } from 'antd';
+import ReactDOM from 'react-dom/client';
+
+import '@/assets/styles/tailwind.css';
+
+const rootEL = document.getElementById('root');
+
+const App = () => {
+	return <div></div>;
+};
+if (rootEL) {
+	const root = ReactDOM.createRoot(rootEL);
+	root.render(
+		<ConfigProvider
+			theme={{
+				token: {
+					colorPrimary: '#7c3aed',
+					colorPrimaryBg: '#f8f0ff',
+					colorPrimaryBgHover: '#f0e0ff',
+					colorPrimaryBorder: '#d9b8ff',
+					colorPrimaryBorderHover: '#bf8fff',
+					colorPrimaryHover: '#a064fa',
+					colorPrimaryActive: '#5b26c7',
+					colorPrimaryTextHover: '#a064fa',
+					colorPrimaryText: '#7c3aed',
+					colorPrimaryTextActive: '#5b26c7'
+				}
+			}}
+		>
+			<App />
+		</ConfigProvider>
+	);
+}
